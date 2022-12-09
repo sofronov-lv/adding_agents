7ef8a1d8e35bb145b32f29bb2e73e1d551d2bcfd
ktdf <woolf.dr@mail.ru>
2022-12-09 13:45:29 +0300

    Implementation of adding a user to the site

--- authorization_psb.py
+++ authorization_psb.py
@@ -1,11 +1,30 @@
+import json
 import requests
-from bs4 import BeautifulSoup
 
 from config import DATA, PSB_URL
 
 session = requests.Session()  # starting a session with the site
 
-authorization_responce = session.post(PSB_URL, data=DATA)  # authorization on the site using the ost method
-profile_responce = session.get("https://lk.psb.services/orders/rko/order")
-profile_responce.encoding = "utf-8"  # explicitly specify the encoding
-print(profile_responce.text)
+response = session.post(PSB_URL, data=DATA)  # authorization on the site using the ost method
+json_dict = json.loads(response.text)
+token = json_dict["access_token"]  # get a token for subsequent work
+headers = {"Authorization": f"Bearer {token}"}
+
+email = "s896_client_bank_test_3@mail.ru"
+fio = "ХАНВЕРДИЕВ ДАВИД АРСЕНОВИЧ"
+inn = "500121199264"
+phone_number = "9857604284"
+
+user_data = {
+    "city_id": 1,  # int
+    "email": email,
+    "fio": fio,
+    "inn": inn,  # str
+    "open_payment_account": True,
+    "organization_name": f"ип {fio}",
+    "phone": "7" + phone_number  # str
+}
+
+res = session.post("https://api.lk.psb.services/agent/v1/rko/order", data=user_data, headers=headers)
+res.encoding = "utf-8"  # explicitly specify the encoding
+print(res.text)
--- config.py
+++ config.py
@@ -2,9 +2,13 @@ LOGIN = "s89647237722@yandex.ru"
 PASSWORD = "Privet1994!"
 
 DATA = {
+    "client_id": "testclient",
+    "client_secret": "testpass",
+    "grant_type": "password",
     "username": LOGIN,
     "password": PASSWORD
 }
 
-PSB_URL = "https://lk.psb.services/"
+PSB_URL = "https://api.lk.psb.services/agent/oauth2/token"
 URALSIB_URL = "https://www.uralsib.ru/business/sotrudnichestvo/login"
+# Exapmle mail s896_client_bank_test_1@mail.ru
